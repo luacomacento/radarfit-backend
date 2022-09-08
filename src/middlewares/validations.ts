9a461b8daee4fe54f156f9f120e4350d5117f5e2
luacomacento <luacomacento@gmail.com>
2022-09-08 17:03:34 -0300

    :recycle:refactor: implements async validations

--- src/middlewares/validations.ts
+++ src/middlewares/validations.ts
@@ -2,28 +2,26 @@ import { NextFunction, Request, Response } from "express";
 import Joi from 'joi';
 
 class Validation {
-  public validateFullProduct(req: Request, res: Response, next: NextFunction) {
+  public async validateFullProduct(req: Request, res: Response, next: NextFunction) {
     const schema = Joi.object({
       produto: Joi.string().required(),
       valor: Joi.number().required(),
       descricao: Joi.string().required(),
     });
 
-    const { error } = schema.validate(req.body);
-    if (error) throw new Error(error.details[0].message);
+    await schema.validateAsync(req.body);
   
     next();
   };
 
-  public validatePartialProduct(req: Request, res: Response, next: NextFunction) {
+  public async validatePartialProduct(req: Request, res: Response, next: NextFunction) {
     const schema = Joi.object({
       produto: Joi.string(),
       valor: Joi.number(),
       descricao: Joi.string(),
     });
 
-    const { error } = schema.validate(req.body);
-    if (error) throw new Error(error.details[0].message);
+    await schema.validateAsync(req.body);
   
     next();
   };
